d660b18261c7afb04cc8b822c9afc132b5dc0746
crawfordforbes <crawford.forbes@gmail.com>
2014-10-29 16:51:20 -0400

    heroku testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require dashboard
 //= require_tree .
